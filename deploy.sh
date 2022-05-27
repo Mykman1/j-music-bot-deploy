37515b5299921216a38d0cc80dd106aab36923f0
Mykolas Blažonis <mykolaas@gmail.com>
2022-05-28 01:19:23 +0300

    Simplify deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,7 @@
 export TOKEN=$(gcloud secrets versions access latest --secret=j-music-bot-token)
 export OWNER=$(gcloud secrets versions access latest --secret=j-music-bot-owner)
 
-echo "token = ${TOKEN}" > config.txt
-echo "owner = ${OWNER}" >> config.txt
-echo "prefix = \"!\"" >> config.txt
+echo "$(envsubst < config_base.txt)" > config.txt
 
 docker stop jmusicbot
 docker rm $(docker ps -a -q -f status=exited)
