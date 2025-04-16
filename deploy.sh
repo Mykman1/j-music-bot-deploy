8ddef908723ecffef905e3777182bbf90eeb3fef
Mykolas Blažonis <mykolaas@gmail.com>
2025-04-16 22:42:15 +0300

    Use a fork

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 TOKEN="$(gcloud --project=370365808701 secrets versions access latest --secret=j-music-bot-token)"
 OWNER="$(gcloud --project=370365808701 secrets versions access latest --secret=j-music-bot-owner)"
-DOWNLOAD_URL="$(wget -qO- https://api.github.com/repos/jagrosh/MusicBot/releases/latest \
+DOWNLOAD_URL="$(wget -qO- https://api.github.com/repos/SeVile/MusicBot/releases/latest \
     | jq -r .assets[].browser_download_url)"
 export TOKEN OWNER DOWNLOAD_URL
 docker compose up -d --build --force-recreate
